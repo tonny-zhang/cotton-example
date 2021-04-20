9d088c3d65e3c9ee5a132af0e176fe25a6d7310a
tonnyzhang <wodexintiao@gmail.com>
2021-04-20 09:39:55 +0800

    support redis session

--- session/main.go
+++ session/main.go
@@ -14,11 +14,15 @@ func main() {
 		ctx.String(http.StatusInternalServerError, fmt.Sprintf("[ERROR] %v", err))
 	}))
 	router.Use(cotton.Logger())
-	// router.Use(session.Middleware(""))
+
+	// use memory session
 	g := router.Group("/session")
-	g.Use(session.Middleware(""))
+	mgrMemory := session.NewMemoryMgr()
+	g.Use(session.Middleware(mgrMemory))
+
 	g.Post("/set", func(ctx *cotton.Context) {
-		session.GetSession(ctx).Set("user", "tonny")
+		ss := session.GetSession(ctx)
+		ss.Set("user", ctx.GetPostForm("user"))
 		ctx.String(http.StatusOK, "writed")
 	})
 	g.Get("/get", func(ctx *cotton.Context) {
@@ -35,12 +39,53 @@ func main() {
 		ctx.String(http.StatusOK, "delete")
 	})
 
+	// use redis session
+	mgrRedis := session.NewRedisMgr("localhost:6379", 0, "")
+	gRedis := router.Group("/redis")
+	gRedis.Use(session.Middleware(mgrRedis))
+	gRedis.Post("/set", func(ctx *cotton.Context) {
+		ss := session.GetSession(ctx)
+		ss.Set("user", ctx.GetPostForm("user"))
+		ss.Save()
+		ctx.String(http.StatusOK, "writed")
+	})
+	gRedis.Get("/get", func(ctx *cotton.Context) {
+		v, e := session.GetSession(ctx).Get("user")
+		if e == nil {
+			ctx.String(http.StatusOK, v.(string))
+		} else {
+			ctx.String(http.StatusOK, "not found")
+		}
+	})
+	gRedis.Get("/delete", func(ctx *cotton.Context) {
+		ss := session.GetSession(ctx)
+		ss.Del("user")
+		ss.Save()
+
+		ctx.String(http.StatusOK, "delete")
+	})
+
+	// no use session with panic
 	router.Get("/nosession", func(ctx *cotton.Context) {
 		v, e := session.GetSession(ctx).Get("user")
 		if e == nil {
 			ctx.String(http.StatusOK, v.(string))
 		} else {
 			ctx.String(http.StatusOK, "not found")
+		}
+	})
+	// no use session with not panic
+	router.Get("/nosession/nopanic", func(ctx *cotton.Context) {
+		if session.HasUsedSession(ctx) {
+			ss := session.GetSession(ctx)
+			v, e := ss.Get("user")
+			if e == nil {
+				ctx.String(http.StatusOK, v.(string))
+			} else {
+				ctx.String(http.StatusOK, "not found")
+			}
+		} else {
+			ctx.String(http.StatusOK, "not use session")
 		}
 	})
 	router.Run("")
